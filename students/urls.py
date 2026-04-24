8418defcc2917fae26896813a887377df2f60058
eissa0001 <3eissa333@gmail.com>
2026-04-24 18:47:49 +0300

    fix create admin import

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import take_attendance, trainer_dashboard, attendance_report, dashboard
 from django.contrib.auth import views as auth_views
 from .views import export_attendance_excel
-from .views import create_admin
+from students.views import create_admin
 urlpatterns = [
 
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
